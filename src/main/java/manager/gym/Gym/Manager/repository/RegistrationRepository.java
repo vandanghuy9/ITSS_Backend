8aa42e85e5f3119b1e682563e46aeb2659e25b4b
vananh0110 <phamvananh1102002@gmail.com>
2023-06-27 08:17:52 +0700

    add RegistrationService

--- src/main/java/manager/gym/Gym/Manager/repository/RegistrationRepository.java
+++ src/main/java/manager/gym/Gym/Manager/repository/RegistrationRepository.java
@@ -1,11 +1,9 @@
 package manager.gym.Gym.Manager.repository;
 
-import jakarta.persistence.Table;
 import manager.gym.Gym.Manager.entity.Registration;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-@Table(name = "REGISTRATION")
 public interface RegistrationRepository extends JpaRepository<Registration, Integer> {
 }
